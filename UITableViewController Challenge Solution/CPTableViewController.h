ee462114dddaa090330ea5de553ffbe3c601a169
SP <sp@SPs-Mac-Pro.local>
2014-09-11 13:11:44 -0700

    solve hte challeneg

---
+++ UITableViewController Challenge Solution/CPTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by SP on 9/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPTableViewController : UITableViewController
+
+@end
